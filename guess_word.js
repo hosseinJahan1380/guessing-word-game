292827eba88b900d158f57d70393179837557c3f
hossein <hosseinjahanbanifar@gmail.com>
2025-04-16 07:57:49 +0330

    Enable updating the count of incorrect letters

--- guess_word.js
+++ guess_word.js
@@ -1,8 +1,10 @@
 
-const buttons = document.querySelectorAll('.section.active button');
+const buttons_word = document.querySelectorAll('.section.active button');
 const input = document.querySelector("input")
 const guess_button = document.querySelector(".Guess")
 const false_buttons = document.querySelectorAll(".box_False_letters button")
+const remaining_false_letters = document.querySelector(".remaining_false_letters")
+const next_level_button = document.querySelector(".next")
 
 // enter a character to guess
 input.addEventListener( "input", ()=>{
@@ -15,25 +17,49 @@ input.addEventListener( "input", ()=>{
 guess_button.addEventListener("click" , ()=>{
     existence_letter = false;
     if ( input.value !="" ){
-        console.log("guess : " , input.value)
         guess_value = input.value.trim().toLowerCase()  // the letter we guess
-        buttons.forEach(btn =>{
+
+        buttons_word.forEach(btn =>{
             letter = btn.textContent.trim().toLowerCase()
-            console.log("the letter is " , letter)
+
             console.log(guess_value === letter)
             // compare the letter we enter with the letters inside buttons
             if (letter === guess_value){
-                btn.style.color = "black"
+                btn.style.color = "#333"
+                btn.style.backgroundColor = "#fff"
                 existence_letter = true
             }  
+            
         })
-        false_buttons.forEach(btn=>{
-            if (btn.textContent ==""){
-                btn.textContent = guess_value
-                btn.style.color = "red"
-                btn.style.borderBottom = "1px solid red"
+        var number_fill = 0
+        for (let i =0  ; i<buttons_word.length ; i++){
+            btn = buttons_word[i]
+            if (btn.style.color==="#333"){
+                number_fill++;
             }
-        })
+        }
+        console.log("the number of fill buttons :" , number_fill)
+        // check if all buttons word are fill or not
+        if (number_fill == buttons_word.length){
+            next_level_button.style.display = "inline-block"
+        }
+        
+        if(existence_letter==false){
+            for (let i=0 ; i<false_buttons.length ;i++) {
+                btn = false_buttons[i]
+                if (btn.textContent===""){
+                    console.log("no letter")
+                    // Update the number of remaining false letters
+                    remaining_false_letters.textContent--;
+           
+                   
+                    btn.textContent = guess_value;
+                    btn.style.color = "red";
+                    btn.style.borderBottom = "1px solid red";
+                    break;
+                }
+            }
+        }
     }
 })
 
